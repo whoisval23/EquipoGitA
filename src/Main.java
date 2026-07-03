30bb08e0cf5f829730adfb33592ac36912a17294
Angeladriand059 <angeladriand059gmail.com>
2026-07-03 13:21:27 -0600

    JIJIJA

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ void main() {
     System.out.println("Alejandro is here");
     System.out.println("Said Alejandro");
     System.out.println("Alejandro Villarrel");
-    System.out.println("Adrian tiene down");
+    System.out.println("Adrian se la mete a Alejandro");
     }
 
 
